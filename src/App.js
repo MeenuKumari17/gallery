e57dc5a08d2351cf374e32839013a6f492cfdff1
meenukumari98 <102102221+meenukumari98@users.noreply.github.com>
2022-06-16 23:20:02 +0530

    16-06-22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const imageHandler = (e) => {
+    // console.log(e.target.files);
+    if(e.target.files) {
+      const fileArray = Array.from(e.target.files).map((file) => URL.createObjectURL(file))
+      // console.log(fileArray)
+
+      setSelectedImages((prevImages) => prevImages.concat(fileArray));
+    }
+  }
+
+  const renderImages = (s) => {
+    return s.map((image) => {
+      return <img src={image} key={image}/>
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="heading">
+        ...Mini Gallery...
+      </div>
+      <div>
+        <input type="file" multiple id="file" onChange={imageHandler}/>
+        <div className="label-holder">
+          <label htmlFor="file" className="label">
+            <p className='tag'>Select Image</p>
+          </label>
+        </div>
+        <div className="result">
+          {renderImages( selectedImages)}
+        </div>
+      </div>
     </div>
   );
 }
